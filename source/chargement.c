009bcc3ca0c8a41595bc6be80cb065f585555685
amine-lbabda <mohamedamine.lbabda@esprit.tn>
2025-05-11 19:25:34 +0100

    Fixing the seg fault

--- source/chargement.c
+++ source/chargement.c
@@ -15,52 +15,8 @@ void chargement(SDL_Surface *ecran, int *run, int *initial_menu)
     int indice = 0;
     init_save(&s);
     while (quitter && *run && *initial_menu)
-    {
-
-        
-
+    {            afficher_sous_menu(s, ecran);
             while (SDL_PollEvent(&event) && *run)
-            {
-                switch (event.type)
-                {
-                case SDL_QUIT:
-                    quitter = 0;
-                    *run = 0;
-                    break;
-                case SDL_KEYDOWN:
-                {
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                    {
-                        quitter = 0;
-                        *run = 0;
-                    }
-                }
-                break;
-                case SDL_MOUSEBUTTONDOWN:
-                    if (event.button.button == SDL_BUTTON_LEFT)
-                    {
-                        miseajour(&s);
-                        if (s.btn_select == 5)
-                        {
-                            *initial_menu = 0;
-                        }
-                        if (s.btn_select == 1)
-                        {
-                            indice = 1;
-                        }
-                        if (s.btn_select == 2)
-                        {
-                            quitter = 0;
-                        }
-                    }
-                    break;
-                }
-            }
-
-
-        
-            afficher_sous_menu(s, ecran);
-            while (SDL_PollEvent(&event))
             {
                 switch (event.type)
                 {
@@ -80,7 +36,10 @@ void chargement(SDL_Surface *ecran, int *run, int *initial_menu)
                         {
                             printf("New Game sélectionné\n");
                             start_tache(ecran,run,initial_menu);
-                            Mix_FreeMusic(s.continu);
+                            if (s.continu) {
+                                Mix_FreeMusic(s.continu);
+                                s.continu = NULL;
+                            }
                             s.continu = Mix_LoadMUS("../assets/audio/theme2.mp3");
                             Mix_PlayMusic(s.continu, -1);
                         }
@@ -100,10 +59,8 @@ void chargement(SDL_Surface *ecran, int *run, int *initial_menu)
                 }
                 break;
             }
-
-        
         SDL_Flip(ecran);
     }
     liberer_save(&s);
-    *initial_menu = 0; //TODO:fix the save issue
+    *initial_menu = 0; 
 }
